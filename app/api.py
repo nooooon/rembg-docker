d9b4921000253ab85b0df5672a8bbf86041ca79f
keishu <hnzero24@gmail.com>
2021-06-11 14:55:16 +0900

    Exception handling

--- app/api.py
+++ app/api.py
@@ -21,27 +21,32 @@ def upload():
     stream = request.files['image'].stream
     img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
 
-    result = remove(img_array,
-        alpha_matting=True,
-        alpha_matting_foreground_threshold=240,
-        alpha_matting_background_threshold=10,
-        alpha_matting_erode_structure_size=4)
-    img = Image.open(io.BytesIO(result)).convert("RGBA")
+    try:
+      result = remove(img_array,
+          model_name="u2net",
+          alpha_matting=True,
+          alpha_matting_foreground_threshold=240,
+          alpha_matting_background_threshold=10,
+          alpha_matting_erode_structure_size=4)
+      img = Image.open(io.BytesIO(result)).convert("RGBA")
 
-    # local save
-    if not os.path.exists(outDir):
-      os.makedirs(outDir)
-    output_path = outDir + '/cutout.png'
-    img.save(output_path)
+      # local save
+      if not os.path.exists(outDir):
+        os.makedirs(outDir)
+      output_path = outDir + '/cutout.png'
+      img.save(output_path)
 
-    # byte data
-    buffer = io.BytesIO()
-    img.save(buffer, format='PNG')
-    binaryData = "data:image/png;base64," + base64.b64encode(buffer.getvalue()).decode()
+      # byte data
+      buffer = io.BytesIO()
+      img.save(buffer, format='PNG')
+      binaryData = "data:image/png;base64," + base64.b64encode(buffer.getvalue()).decode()
 
-    return render_template('index.html', image=binaryData)
-    # return redirect('/')
+      return render_template('index.html', image=binaryData)
+      # return redirect('/')
 
+    except Exception as e:
+      print(e)
+      return render_template('index.html')
 
 
 if __name__ == '__main__':
